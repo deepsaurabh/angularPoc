2fbb7323c04f9d8290fd6d390123a3971b19d8ca
unknown <deep.singh@NOICLT264.imfcorp.com>
2014-09-12 15:31:30 +0530

    Final poc

---
+++ app/directives/treeDirective.js
@@ -0,0 +1,21 @@
+﻿app.directive('map', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<ul ng-repeat="brand in locations"> <li ng-repeat="(brandName , products) in brand"">{{brandName}} <ul ng-repeat="x in products"><a href = "#/customers/{{x.id}}">{{x.product}}</a></ul></li>  </ul>',
+        scope: {
+            locations: '=locations'
+        },
+        link: function(scope, element, attrs) {
+//            scope.$watch('locations', function(locations) {
+//                angular.forEach(locations, function(location, key) {
+//                    console.log(location)
+//                });
+//            });
+            scope.abc = function(id){
+                alert(id);
+                // $state.go('customers/:id')
+            }
+        }
+    };
+});
---
+++ app/services/Service.js
@@ -0,0 +1,27 @@
+﻿
+app.service('customersService', function () {
+    this.getBrands = function () {
+    
+        return brand;
+    };
+
+    this.getProduct = function (id) {
+        for (var i = 0; i < product.length; i++) {
+            if (product[i].id == id) {
+                return product[i];
+            }
+        }
+        return null;
+    };
+    var brand = [{'samsung':[{'id':'1','product':'Fridge'},{'id':'2','product':'Microwave'},{'id':'3','product':'Telivision'}]},{'Lg':[{'id':'4','product':'Air Conditioner'},{'id':'5','product':'Washing Machine'},{'id':'6','product':'Television'}]}];
+
+    var product = [{'id':'1','product':'Fridge','Brand':'Samsung','Description':'aaaaaaaaaaaaaaaaaaaaaaaaaaaa'},
+                     {'id':'2','product':'Microwave','Brand':'Samsung','Description':'bbbbbbbbbbbbbbbbbbbbbbb'},
+                     {'id':'3','product':'Telivision','Brand':'Samsung','Description':'cccccccccccccccccccccccccccc'},
+                     {'id':'4','product':'Air Conditioner','Brand':'Lg','Description':'ddddddddddddddddddddddddd'},
+                     {'id':'5','product':'Waschig Machine','Brand':'Lg','Description':'eeeeeeeeeeeeeeeeeeeee'},
+                     {'id':'6','product':'Television','Brand':'Lf','Description':'ffffffffffffffffffffff'}
+                    ];
+   
+
+});
